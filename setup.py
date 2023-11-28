84e2b892c5648a9cd28e4086516ad4c7cfe0e890
nicmostert <nicolas.mostert@horizons.govt.nz>
2023-11-28 14:06:24 +1300

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/nicmostert/annalist.git",
-    version="0.3.3",
+    version="0.3.4",
     zip_safe=False,
 )
